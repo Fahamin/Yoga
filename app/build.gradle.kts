c41a4452cfb6b6a9db9b09adc5aa6a3a99f83e24
fahamin <fahamin00@gmail.com>
2025-05-23 12:47:02 +0600

    v add

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.livetv.configurator.nexus.kodiapps"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 23
+        versionName = "2.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
